cfa1996184e8d2bc474b3c4537b1c0a32d62d539
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-07-21 16:18:15 +0900

    Pera.hlsliがインクルードできない

--- Pera.VS.hlsl
+++ Pera.VS.hlsl
@@ -1,13 +1,13 @@
-//#include "Pera.hlsli"
+#include "Pera.hlsli"
 
-Texture2D<float4> tex : register(t0); // 通常テクスチャ
-SamplerState smp : register(s0); // サンプラー
+//Texture2D<float4> tex : register(t0); // 通常テクスチャ
+//SamplerState smp : register(s0); // サンプラー
 
-struct Output
-{
-    float4 svpos : SV_POSITION;
-    float2 uv : TEXCOORD;
-};
+//struct Output
+//{
+//    float4 svpos : SV_POSITION;
+//    float2 uv : TEXCOORD;
+//};
 
 struct VertexShaderInput
 {
