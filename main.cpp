8d5c2cdb9f8a7fa5f75fbdc49ab6b4d6f17892c5
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-12-03 09:23:29 +0900

    リリースチェック

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@
 
 #pragma comment(lib,"dxguid.lib")
 
+struct ResourceLeackChecker {
+	~ResourceLeackChecker() {
+		Microsoft::WRL::ComPtr<IDXGIDebug1> debug;
+		if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+			debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+			debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+			debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+		}
+	}
+};
+
+static ResourceLeackChecker leakCheck;
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
